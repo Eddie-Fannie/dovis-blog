93cf35043c719a7269c3af55e19679a59b8fdec1
Eddie-Fannie <1597271709@qq.com>
2020-05-12 20:06:50 +0800

    修改logo路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,10 @@ module.exports = {
     title: 'Dovis\'s blog',
     description: '前端学习,天道酬勤',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-        ['link', { rel: 'icon', href: ' http://wx.qlogo.cn/mmopen/4K4oWQStIpFZgsNhspicuR2b62TaWEgFp9iaUDwKGOiaxNDQwKkWJ7RgqNjx2Ctib3g1oz69dUdhc1Ilf8vIs6JzJ0Ve9oFZWyiaic/64' }], // 增加一个自定义的 favicon(网页标签的图标)
-        // ['link', { rel: 'manifest', href: ' http://wx.qlogo.cn/mmopen/4K4oWQStIpFZgsNhspicuR2b62TaWEgFp9iaUDwKGOiaxNDQwKkWJ7RgqNjx2Ctib3g1oz69dUdhc1Ilf8vIs6JzJ0Ve9oFZWyiaic/64' }],
-        ['link', { rel: 'apple-touch-icon', href: ' http://wx.qlogo.cn/mmopen/4K4oWQStIpFZgsNhspicuR2b62TaWEgFp9iaUDwKGOiaxNDQwKkWJ7RgqNjx2Ctib3g1oz69dUdhc1Ilf8vIs6JzJ0Ve9oFZWyiaic/64' }],
-        ['link', { rel: 'mask-icon', href: ' http://wx.qlogo.cn/mmopen/4K4oWQStIpFZgsNhspicuR2b62TaWEgFp9iaUDwKGOiaxNDQwKkWJ7RgqNjx2Ctib3g1oz69dUdhc1Ilf8vIs6JzJ0Ve9oFZWyiaic/64', color: '#3eaf7c' }],
+        ['link', { rel: 'icon', href: '/logo.jpeg' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', { rel: 'manifest', href: '/logo.jpeg' }],
+        ['link', { rel: 'apple-touch-icon', href: '/logo.jpeg' }],
+        ['link', { rel: 'mask-icon', href: '/logo.jpeg', color: '#3eaf7c' }],
         ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache' }],
         ['meta', { 'http-quiv': 'expires', cotent: '0' }],
         ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache, must-revalidate' }]
