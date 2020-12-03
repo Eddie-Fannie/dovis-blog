31e9fefb29211583cb6c079f12c63dffa708140e
Eddie-Fannie <1597271709@qq.com>
2020-12-03 12:45:56 +0800

    增加自动生成侧边目录的配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,14 +5,14 @@ module.exports = {
     description: '前端学习,天道酬勤',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', { rel: 'icon', href: '/logo.jpeg' }], // 增加一个自定义的 favicon(网页标签的图标)
-        // ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['link', { rel: 'apple-touch-icon', href: '/logo.jpeg' }],
         ['link', { rel: 'mask-icon', href: '/logo.jpeg', color: '#3eaf7c' }],
         ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache' }],
         ['meta', { 'http-quiv': 'expires', cotent: '0' }],
         ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache, must-revalidate' }]
     ],
-    serviceWorker: true, // 是否开启 PWA
+    // serviceWorker: true, // 是否开启 PWA
     base: '/dovis-blog/', // 这是部署到github相关的配置
     markdown: {
         lineNumbers: true // 代码块显示行号
@@ -26,11 +26,12 @@ module.exports = {
         collapsable: false
     },
     plugins: [
-        // '@vuepress/pwa', {
-        //     serviceWorker: true,
-        //     updatePopup: true
-        // },
-        '@vuepress/back-to-top'
+        '@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: true
+        },
+        '@vuepress/back-to-top',
+        'autobar'
     ]
     // plugins: {
     //     '@vuepress/pwa': {
@@ -40,6 +41,6 @@ module.exports = {
     //             buttonText: "Refresh"
     //         }
     //     },
-    //     '@vuepress/back-to-top'
+    //     // '@vuepress/back-to-top'
     // }
 };
